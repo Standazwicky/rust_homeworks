59219c865c42a315b1228949cdb09da5ef7de318
Stanislav Paláček <stanislav.palacek@vsb.cz>
2024-04-26 16:05:26 +0200

    2.ukol
    Řešení 2. úkolu.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,77 @@
-use std::time::SystemTime;
+use heck::AsSnakeCase;
+use heck::AsUpperCamelCase;
+use slug::slugify;
+use std::env;
+use std::io;
 
 fn main() {
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-     Ok(n) => println!("Hello! It's been {} seconds since 1970-01-01 00:00:00 UTC",n.as_secs()),
-     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        help();
+    } else {
+        println!("Enter the text you want to convert:");
+
+        match args[1].as_str() {
+            "lowercase" => {
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line");
+                println!("{}", text.to_lowercase());
+            }
+            "uppercase" => {
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line");
+                println!("{}", text.to_uppercase());
+            }
+            "no-space" => {
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line");
+                println!("{}", text.replace(" ", ""));
+            }
+            "slugify" => {
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line");
+                println!("{}", slugify(text));
+            }
+            "camelcase" => {
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line");
+                println!("{}", AsUpperCamelCase(text));
+            }
+            "snakecase" => {
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line");
+                println!("{}", AsSnakeCase(text));
+            }
+            _ => {
+                println! {"Wrong input argument."};
+                println! {};
+                help();
+            }
+        }
     }
+}
+
+fn help() {
+    println!("Use one input argument.");
+    println!("");
+    println!("Options:");
+    println!("lowercase - convert the entire text to lowercase");
+    println!("uppercase - convert the entire text to uppercase");
+    println!("no-spaces - remove all spaces from the text");
+    println!("slugify - convert the text into a slug");
+    println!("camelcase - convert the text into a UpperCamelCase");
+    println!("snakecase - convert the text into a snake_case");
 }
